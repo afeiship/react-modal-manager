10147962d4fb695ba2685b38d0938f71a077de81
afeiship <1290657123@qq.com>
2024-01-16 09:38:15 +0800

    fix: typing err

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,9 @@
 interface NxStatic {
   $modal: {
-    present: (inName: string, inData?: any) => Promise<void>;
-    presentWhen: (inName: string, inData?: any) => Promise<void>;
-    dismiss: (inName: string) => Promise<void>;
-    dismissAll: (inNames: string[]) => Promise<any>;
+    present: (inName: string, inData?: any) => Promise<void> | void;
+    presentWhen: (inName: string, inData?: any) => Promise<void> | void;
+    dismiss: (inName: string) => Promise<void> | void;
+    dismissAll: (inNames: string[]) => Promise<any> | void;
     value: (inName: string) => { visible: boolean; data: any };
     visible: (inName: string) => boolean;
     data: (inName: string) => any;
