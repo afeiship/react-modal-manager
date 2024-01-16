64726a5680762109b3222e88e5f51a8495e905b6
afeiship <1290657123@qq.com>
2024-01-16 09:41:15 +0800

    fix: return type err

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,9 @@
 interface NxStatic {
   $modal: {
-    present: (inName: string, inData?: any) => Promise<void> | void;
-    presentWhen: (inName: string, inData?: any) => Promise<void> | void;
-    dismiss: (inName: string) => Promise<void> | void;
-    dismissAll: (inNames: string[]) => Promise<any> | void;
+    present: (inName: string, inData?: any) => (Promise<void> | void);
+    presentWhen: (inName: string, inData?: any) => (Promise<void> | void);
+    dismiss: (inName: string) => (Promise<void> | void);
+    dismissAll: (inNames: string[]) => (Promise<any> | void);
     value: (inName: string) => { visible: boolean; data: any };
     visible: (inName: string) => boolean;
     data: (inName: string) => any;
