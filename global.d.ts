c22a86a295816f6c39ed5d3bb3a567ccb6825016
afeiship <1290657123@qq.com>
2024-01-16 09:55:15 +0800

    fix: nams optional typing err

--- global.d.ts
+++ global.d.ts
@@ -3,7 +3,7 @@ interface NxStatic {
     present: (inName: string, inData?: any) => (Promise<void> | void);
     presentWhen: (inName: string, inData?: any) => (Promise<void> | void);
     dismiss: (inName: string) => (Promise<void> | void);
-    dismissAll: (inNames: string[]?) => (Promise<any> | void);
+    dismissAll: (inNames?: string[]) => (Promise<any> | void);
     value: (inName: string) => { visible: boolean; data: any };
     visible: (inName: string) => boolean;
     data: (inName: string) => any;
