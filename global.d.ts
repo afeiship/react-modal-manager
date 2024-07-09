ea720fe8d89afbfd8fe584a2f0bf12d34e8b997d
afeiship <1290657123@qq.com>
2024-07-09 21:44:08 +0800

    fix: typing

--- global.d.ts
+++ global.d.ts
@@ -3,9 +3,9 @@ interface NxStatic {
     present: (inName: string, inData?: any) => (Promise<void> | void);
     presentWhen: (inName: string, inData?: any) => (Promise<void> | void);
     dismiss: (inName: string) => (Promise<void> | void);
-    dismissAll: (inNames?: string[]) => (Promise<any> | void);
+    dismissAll: (inNames?: string[] | any) => (Promise<any> | void);
     value: (inName: string) => { visible: boolean; data: any };
     visible: (inName: string) => boolean;
     data: (inName: string) => any;
-  }
+  };
 }
